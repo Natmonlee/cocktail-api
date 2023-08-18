622d8adddded38bd1ef707e6b4941175b843e8f7
Natalie Moran <nataliemoran89@live.co.uk>
2023-08-18 20:57:08 +0100

    Add search functionality

--- src/index.js
+++ src/index.js
@@ -4,19 +4,98 @@ const app = express()
 const port = 5000
 const fs = require("fs");
 const papa = require('papaparse')
-let database;
+let cocktailDatabase;
+let formattedDatabase = [];
+let searchName;
+let searchAlcoholic;
+let searchGlass;
+let searchIngredients;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const formatRecipe = (rawRecipe) => {
+  let recipe = {};
+  recipe.name = rawRecipe.Name;
+  recipe.alcoholic = rawRecipe.Alcoholic;
+  recipe.glass = rawRecipe.Glass;
+  recipe.photoUrl = rawRecipe.Photo;
+  let ingredients = {};
+  for (let i = 0; i < 16; i++) {
+      for (const [key, value] of Object.entries(rawRecipe)){
+          if (key === 'Ingredient ' + i) {
+              if (value) {
+                  ingredients[value] = rawRecipe['Measurement ' + i];
+              }
+          }      
+      }
+  }
+  recipe.ingredients = ingredients;
+  recipe.method = rawRecipe.Method;
+  return recipe;
+} 
+
+const formatDatabase = (database) => {
+  for (const recipe of database) {
+    if (recipe.Name) {
+      formattedDatabase.push(formatRecipe(recipe));
+    }
+  }
+  return formattedDatabase;
+}
 
 fs.readFile("all_drinks.csv", "utf-8", (err, data) => {
   if (err) {
     console.log(err)}
   else {
-    database = papa.parse(data, {header:true});
+    cocktailDatabase = papa.parse(data, {header:true});
+    formatDatabase(cocktailDatabase.data);
 }
 })
 
+const generateResults = (database) => {
+
+  const checkName = (inputName, recipe) => {
+    if (inputName === 'undefined') {
+      return true;
+    }
+    else {
+      return recipe.name.toLowerCase().includes(inputName);
+    }
+  };
+
+  const checkAlcohol = (inputAlcohol, recipe) => {
+    if (inputAlcohol === 'undefined') {
+      return true;
+    }
+    else {
+      let alcoholPreference = 'Alcoholic';
+    if (inputAlcohol === 'false') {
+      alcoholPreference = 'Non alcoholic';
+    }
+    return recipe.alcoholic === alcoholPreference;
+    }
+  };
+
+  const checkGlass = (inputGlass, recipe) => {
+    if (inputGlass === 'undefined') {
+      return true;
+    }
+    else {
+      return recipe.glass.toLowerCase().includes(inputGlass);
+    }
+  };
+
+  // INGREDIENTS CHECK
+
+  let results = [];
+  for (const cocktail of database) {
+    if (checkName(searchName, cocktail) && checkAlcohol(searchAlcoholic, cocktail) && checkGlass(searchGlass, cocktail)) {
+      results.push(cocktail);
+    }
+  }
+  return results;
+} 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -26,56 +105,15 @@ app.use(function(req, res, next) {
   next();
 });
 
-let searchName;
-let searchAlcoholic;
-let searchGlass;
-let searchIngredients;
-
-const generateResults =  (database) => {
-  let results = [];
-  for (const recipe of database) {
-    if (recipe.Name.toLowerCase().includes(searchName.toLowerCase()) === true) {
-      results.push(recipe);
-    }
-  }
-  return results;
-}
-
-
-
-const formatFetchedRecipe = (fetchedRecipe) => {
-  let recipe = {};
-  recipe.name = fetchedRecipe.Name;
-  recipe.alcoholic = fetchedRecipe.Alcoholic === 'Alcoholic' ? true : false;
-  recipe.glass = fetchedRecipe.Glass;
-  recipe.photoUrl = fetchedRecipe.Photo;
-  let ingredients = {};
-  for (let i = 0; i < 16; i++) {
-      for (const [key, value] of Object.entries(fetchedRecipe)){
-          if (key === 'Ingredient ' + i) {
-              if (value) {
-                  ingredients[value] = fetchedRecipe['Measurement ' + i];
-              }
-          }      
-      }
-  }
-  recipe.ingredients = ingredients;
-  recipe.method = fetchedRecipe.Method;
-  return recipe;
-}
-
-
-app.get('/', (req, res) => {
-  //const results = formatFetchedRecipe(database.data[search]);
-  searchName = req.query.name;
-  searchAlcoholic = req.query.alcoholic;
-  searchGlass = req.query.glass;
-  searchIngredients = req.query.ingredients;
-  res.send(generateResults(database.data));
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-
+app.get('/', (req, res) => {
+  //const results = formatFetchedRecipe(database.data[search]);
+  searchName = req.query.name.toLowerCase();
+  searchAlcoholic = req.query.alcoholic;
+  searchGlass = req.query.glass.toLowerCase();
+  //searchIngredients = req.query.ingredients.toLowerCase();
+  res.send(generateResults(formattedDatabase));
+})
